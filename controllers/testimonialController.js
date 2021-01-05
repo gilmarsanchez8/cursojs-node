51d5acd486ca616e7bc793db535e3e8efcad4c26
Gilmar Sanchez Cuartas <gilmarsancu@gmail.com>
2021-01-05 12:33:22 -0500

    Ejercicio agencia de viajes con node

---
+++ controllers/testimonialController.js
@@ -0,0 +1,53 @@
+import {Testimonial} from '../models/Testimoniales.js';
+
+const guardarTestimonial = async (req, res) => {
+    //Validar...
+    const {nombre, correo, mensaje} = req.body;
+
+    const errores = [];
+
+    if(nombre.trim() === ''){
+        errores.push({mensaje: 'El nombre esta vacío'});
+    }
+
+    if(correo.trim() === ''){
+        errores.push({mensaje: 'El correo electrónico esta vacío'});
+    }
+
+    if(mensaje.trim() === ''){
+        errores.push({mensaje: 'El mensaje esta vacío'});
+    }
+
+    if(errores.length > 0){
+        //Consultar los testimoniales existentes
+        const testimoniales = await Testimonial.findAll();
+
+        //Mostrar la vista con errores
+        res.render('testimoniales', {
+            pagina: 'Testimoniales',
+            errores,
+            nombre,
+            correo,
+            mensaje,
+            testimoniales
+        })
+    } else {
+        //Almacenar en BD
+        try {
+            await Testimonial.create({
+                nombre,
+                correo,
+                mensaje
+            });
+
+            res.redirect('/testimoniales');
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+export {
+    guardarTestimonial
+}
---
+++ index.js
@@ -0,0 +1,42 @@
+//Antes
+//const express = require('express');
+
+//Con módulos
+import express from 'express';
+import router from './routes/index.js';
+import db from './config/db.js';
+
+const app = express();
+
+//Conectar a la DB
+db.authenticate()
+    .then( () => console.log('BD conectada'))
+    .catch(error => console.log(error));
+
+//Definir puerto
+const port = process.env.PORT || 4000;
+
+//Habilitar PUG
+app.set('view engine', 'pug');
+
+//Obtener año actual
+app.use((req, res, next) => {
+    const year = new Date();
+
+    res.locals.actualYear = year.getFullYear();
+    res.locals.nombreSitio = "Agencia de Viajes";
+    return next();
+})
+
+//Agregar body parser para leer datos del formulario
+app.use(express.urlencoded({extend: true}));
+
+//Definir carpeta publica
+app.use(express.static('public'));
+
+//Agregar router
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`)
+})
